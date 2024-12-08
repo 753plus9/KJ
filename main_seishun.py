6012abdff5e2c6369621c4b593e2ad91ec5d1350
Keiju Hirano <keijuhirano753plus9@gmail.com>
2024-12-08 23:27:01 +0900

    新規ファイル作成

---
+++ main_seishun.py
@@ -0,0 +1,37 @@
+import streamlit as st # フロントエンドを扱うstreamlitの機能をインポート
+from openai import OpenAI # openAIのchatGPTのAIを活用するための機能をインポート
+
+
+# アクセスの為のキーをos.environ["OPENAI_API_KEY"]に代入し、設定
+
+import os # OSが持つ環境変数OPENAI_API_KEYにAPIを入力するためにosにアクセスするためのライブラリをインポート
+# ここにご自身のAPIキーを入力してください！
+os.environ["OPENAI_API_KEY"] = "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"
+
+# openAIの機能をclientに代入
+client = OpenAI()
+
+# chatGPTにリクエストするためのメソッドを設定。引数には書いてほしい内容と文章のテイストと最大文字数を指定
+def run_gpt(content_text_to_gpt):
+    # リクエスト内容を決める
+    request_to_gpt = content_text_to_gpt + "そんな時のおすすめの映画を教えてください。おすすめの映画はTMDBの映画IDで返してください。"
+    
+    # 決めた内容を元にclient.chat.completions.createでchatGPTにリクエスト。オプションとしてmodelにAIモデル、messagesに内容を指定
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": request_to_gpt },
+        ],
+    )
+
+    # 返って来たレスポンスの内容はresponse.choices[0].message.content.strip()に格納されているので、これをoutput_contentに代入
+    output_content = response.choices[0].message.content.strip()
+    return output_content # 返って来たレスポンスの内容を返す
+
+st.write('おすすめ映画アプリ')# タイトル
+
+# 書かせたい内容
+content_text_to_gpt = st.text_input("今日の気分を入力してください！")
+
+output_content_text = run_gpt(content_text_to_gpt)
+st.write(output_content_text)
